28757eddceccb336f44f42e4217dcfc4286fb824
Agustin <tincho082009@gmail.com>
2020-12-02 15:05:24 -0300

    Arreglo repetidos

--- minturMaster/src/App.js
+++ minturMaster/src/App.js
@@ -6,7 +6,6 @@ import "./App.css";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Menu from "./components/Menu";
 import ToTop from "./components/ToTop";
-import Newsletter from "./components/subcomponentes/Newsletter";
 
 import PHome from "./pages/PHome";
 import PZona from "./pages/PZona";
