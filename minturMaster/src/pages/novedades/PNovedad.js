4714b4f71543c3981be2021b42cf46911fd46989
Agustin <tincho082009@gmail.com>
2020-12-09 14:21:42 -0300

    Fix iframe Novedad

--- minturMaster/src/pages/novedades/PNovedad.js
+++ minturMaster/src/pages/novedades/PNovedad.js
@@ -160,7 +160,7 @@ class PNovedad extends Component {
                     ) : (
                       <div className="body">
                         <p className="text-dark mb-2">{descripcion}</p>
-                        <iframe src={`https://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Flocalhost%3A3000%2F%23%2Fnovedad%2F${this.props.match.params.id}&layout=button_count&size=small&width=99&height=20&appId`} width="99" height="20" style={{border: "none", overflow: "hidden"}} scrolling="no" frameBorder="0" allowFullScreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                        <iframe src={`https://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Fturismo.sanluis.gov.ar%2F%23%2Fnovedad%2F${this.props.match.params.id}&layout=button_count&size=small&width=99&height=20&appId`} width="99" height="20" style={{border: "none", overflow: "hidden"}} scrolling="no" frameBorder="0" allowFullScreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
                       </div>
                     )}
                   </div>
