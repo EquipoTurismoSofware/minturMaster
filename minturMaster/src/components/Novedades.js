e641e7b0f3f1a45991c7702870a800e7fd3b1096
programergabyvargas <42458972+programergabyvargas@users.noreply.github.com>
2021-02-08 11:27:46 -0300

    cambio de estilo a Novedades
    ..

--- minturMaster/src/components/Novedades.js
+++ minturMaster/src/components/Novedades.js
@@ -2,8 +2,21 @@ import React, { Component } from "react";
 import { Consumer } from "../context";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { Swiper, SwiperSlide } from "swiper/react";
+import Swiper from "swiper";
 
+const swiper = new Swiper(".blog-slider", {
+  spaceBetween: 30,
+  effect: "fade",
+  loop: true,
+  wheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: ".blog-slider__pagination",
+    clickable: true,
+  },
+});
 class Novedades extends Component {
   constructor(props) {
     super(props);
@@ -15,20 +28,6 @@ class Novedades extends Component {
     };
     this.stop = this.stop.bind(this);
     this.carouselTimer = this.carouselTimer.bind(this);
-
-    /* var swiper = new Swiper(".blog-slider", {
-      spaceBetween: 30,
-      effect: "fade",
-      loop: true,
-      mousewheel: {
-        invert: false,
-      },
-      // autoHeight: true,
-      pagination: {
-        el: ".blog-slider__pagination",
-        clickable: true,
-      },
-    });*/
   }
 
   carouselTimer() {
@@ -62,6 +61,22 @@ class Novedades extends Component {
 
   componentDidMount() {
     //A los datos del Fetch hay que agregar la propiedad display: "none"
+
+    const swiper = new Swiper(".blog-slider", {
+      spaceBetween: 30,
+      effect: "fade",
+      direction: "vertical",
+      loop: true,
+      mousewheel: {
+        invert: false,
+      },
+       autoHeight: true,
+      pagination: {
+        el: ".blog-slider__pagination",
+        clickable: true,
+      },
+    });
+
     var token = this.context.token;
     var self = this;
     axios({
@@ -117,140 +132,91 @@ class Novedades extends Component {
           className=""
           style={{ display: item.display, width: "100%" }}
         >
-          <div className="row">
-            <div className="col-sm-3">
+          <div className="blog-slider__item swiper-slide">
+            <div className="blog-slider__img">
               <img
-                className="img-fluid"
-                style={{ height: "200px" }}
+               
+                style={{ height: "300px" }}
                 src={`${process.env.REACT_APP_API_RECURSOS}/recursos/novedades/${item.foto_uno}`}
                 alt="Foto"
               />
             </div>
+
+            <div class="blog-slider__content">
             <div className="col-sm-9">
               <Link
                 to={`/novedad/${item.id}`}
                 className="link"
-                style={{ color: "white" }}
+                style={{ color: "black" }}
               >
-                <div className="d-flex justify-content-between">
-                  <h1>{item.titulo}</h1>
-                  <span className="pt-3">{`${fecha[2]}/${fecha[1]}/${fecha[0]}`}</span>
-                </div>
+               
+                       <span className="blog-slider__code">{`${fecha[2]}/${fecha[1]}/${fecha[0]}`}</span>
+                     <div className="blog-slider__title">
+                        <h2>{item.titulo}</h2>
+                      </div>
+                      <div><h4>{item.subtitulo}</h4></div>
 
-                <div>
-                  <h4>{item.subtitulo}</h4>
-                </div>
-                <div>
-                  <p>{descripcion}</p>
-                </div>
-                <div className="d-flex justify-content-end">
+                    
+                      <div class="blog-slider__text">
+                        {descripcion}
+                      </div>
+                      <a href={`/novedad/${item.id}`} class="blog-slider__button">
+                          Leer más
+                      </a>
+             {/*}   <div className="d-flex justify-content-end">
                   <i className="fas fa-book-open" />
-                </div>
+                </div> */}
+
+                
+
               </Link>
             </div>
           </div>
+          </div>
+          {
+            // Nuevos-Items
+          }
         </div>
       );
     });
+
     return (
       <div className="container mb-3">
         <div className="row">
           <div className="col">
             <div className="Novedades">
               <Link to="/novedades" className="novedades-leyenda">
-                <div className="background-bar" />
+              {//  <div className="background-bar" />
+              }
                 <div className="texto">
                   <ul>
-                    <li>No</li>
-                    <li>Ve</li>
-                    <li>Da</li>
-                    <li>Des</li>
-                    <li>
-                      <i className="fas fa-arrow-right" />
+                    <li>NoVeDaDes                    
+                     {// <i className="fas fa-arrow-right" />
+                  }   
                     </li>
                   </ul>
                 </div>
               </Link>
-              <div className="novedades-body">{items}</div>
+              
+              <div className="blog-slider">
+                <div class="blog-slider__wrp swiper-wrapper">
+                  <div class="blog-slider__item swiper-slide">{items}</div>
+                </div>
+                <div className="blog-slider__pagination">
+                  <div className="swiper-pagination-bullet-active"></div>
+                  <div className="swiper-pagination-bullet"></div>
+                  <div className="swiper-pagination-bullet"></div>
+                </div>
+        
+              </div>
             </div>
           </div>
         </div>
-        {/*<button type="btn btn-primary" onClick={this.stop}>Stop</button> 
-        <div className="blog-slider">
-          <div class="blog-slider__wrp swiper-wrapper">
-            <div class="blog-slider__item swiper-slide">
-              <div class="blog-slider__img">
-                <img
-                  src="https://i.ibb.co/ft02QmL/SAN-LUIS-TURISMO-FULL-COLOR-TRANSP.png"
-                  alt=""
-                />
-              </div>
-              <div class="blog-slider__content">
-                <span class="blog-slider__code">26 December 2019</span>
-                <div class="blog-slider__title">Lorem Ipsum Dolor</div>
-                <div class="blog-slider__text">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Recusandae voluptate repellendus magni illo ea animi?{" "}
-                </div>
-                <a href="#" class="blog-slider__button">
-                  READ MORE
-                </a>
-              </div>
-            </div>
-            <div class="blog-slider__item swiper-slide">
-              <div class="blog-slider__img">
-                <img
-                  src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759871/jason-leung-798979-unsplash.jpg"
-                  alt=""
-                />
-              </div>
-              <div className="blog-slider__content">
-                <span className="blog-slider__code">26 December 2019</span>
-                <div className="blog-slider__title">Lorem Ipsum Dolor2</div>
-                <div className="blog-slider__text">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Recusandae voluptate repellendus magni illo ea animi?
-                </div>
-                <a href="#" className="blog-slider__button">
-                  READ MORE
-                </a>
-              </div>
-            </div>
 
-            <div className="blog-slider__item swiper-slide">
-              <div className="blog-slider__img">
-                <img
-                  src="http://sanluismidestino.com/wp-content/uploads/2020/11/cropped-ENCABEZADO-2-N.png"
-                  alt=""
-                />
-              </div>
-              <div className="blog-slider__content">
-                <span className="blog-slider__code">26 December 2019</span>
-                <div className="blog-slider__title">Lorem Ipsum Dolor</div>
-                <div className="blog-slider__text">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Recusandae voluptate repellendus magni illo ea animi?
-                </div>
-                <a href="#" class="blog-slider__button">
-                  READ MORE
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="blog-slider__pagination"></div>
+        {/*    <div className="blog-slider">
+          <div class="blog-slider__wrp swiper-wrapper">{items}</div>
         </div>
-        <Swiper
-          spaceBetween={50}
-          slidesPerView={3}
-          onSlideChange={() => console.log("slide change")}
-          onSwiper={(swiper) => console.log(swiper)}
-        >
-          <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          ...
-        </Swiper>*/}
+    */}
       </div>
     );
   }
