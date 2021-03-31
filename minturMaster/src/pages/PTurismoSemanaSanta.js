fff19b934e70b6287a88ddd166eaee78b5e20379
fdamengual <fdamengual@ulp.edu.ar>
2021-03-31 11:35:13 -0300

    Update TurismoSemanaSanta

--- minturMaster/src/pages/PTurismoSemanaSanta.js
+++ minturMaster/src/pages/PTurismoSemanaSanta.js
@@ -1,148 +1,31 @@
 import React, { Component } from "react";
 
-class PTurismoSemanaSanta extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-    };
-  }
-
-  componentDidMount() {
-    console.log("categoria: ");
-  }
-
+export default class PTurismoSemanaSanta extends Component {
   render() {
     return (
       <div className="container mb-5">
         <div className="container">
-          <div className="row mb-3" style={{ paddingTop: "150px" }}>
-            <div className="pantallaGrande">
-              <center>
+          <div
+            className="row mb-3"
+            style={{
+              paddingTop: "150px",
+            }}
+          >
+            {/*<div className="ZonaDetalle-titulo">
+                    <h3 style={{ color: `#722789` }}>Semana Santa en San Luis:</h3>
+                    </div>*/}
+            <center>
+              <div className="row">
                 <img
-                  style={{ height: 250, width: 350 }}
+                  style={{
+                    height: 700,
+                    width: 500,
+                    alignSelf: "center",
+                  }}
                   className="img-fluid"
                   src={`${process.env.REACT_APP_API_RECURSOS}/recursos/FolletoSemanaSanta.jpeg`}
                   alt="Img"
                 />
-                <div className="ZonaDetalle-titulo">
-                  <h3 style={{ color: `#722789` }}>
-                    INFORMACIÓN PARA AFILIADOS:
-                  </h3>
-                </div>
-              </center>
-            </div>
-          </div>
-        </div>
-        <div className="row mb-3" style={{ paddingTop: "0px" }}>
-          <div className="pantallaChica">
-            <center>
-              <img
-                style={{ height: 100, width: 150 }}
-                className="img-fluid"
-                src={`${process.env.REACT_APP_API_RECURSOS}/recursos/LogoDosep.png`}
-                alt="Img"
-              />
-              <div className="ZonaDetalle-titulo">
-                <h3 style={{ color: `#722789` }}>
-                  INFORMACIÓN PARA AFILIADOS:
-                </h3>
-              </div>
-            </center>
-          </div>
-          <div className="pantallaGrande">
-            <div className="ZonaDetalle-titulo ">
-              <h3 style={{ color: `#722789` }}>
-                INFORMACIÓN PARA PRESTADORES DE SERVICIOS:
-              </h3>
-            </div>
-
-            <center>
-              <div className="row">
-                {/*  <div className="one-third column">
-                    <Link to="/listado-guias-covid" className="link-menu">
-                      <img
-                        style={{ height: 200, width: 350 }}
-                        className="img-fluid"
-                        src={`${process.env.REACT_APP_API_RECURSOS}/recursos/modal/TARJETA_EXCURSIONES.jpg`}
-                        alt="Img"
-                      />
-                    </Link>
-    </div>*/}
-                <div className="one-three column">
-                  <a
-                    href="http://turismo.sanluis.gov.ar/dosep-turismo/Bases_Condiciones.pdf"
-                    className="link-menu"
-                  >
-                    <img
-                      style={{ height: 300, width: 550 }}
-                      className="img-fluid"
-                      src={`${process.env.REACT_APP_API_RECURSOS}/recursos/BASESYCONDICIONESDOSEP.png`}
-                      alt="Img"
-                    />
-                  </a>
-                </div>
-                <div className="one-three  column">
-                  <a
-                    href="https://forms.gle/wK9pGbWuNNTc2Z848"
-                    className="link-menu"
-                  >
-                    <img
-                      style={{ height: 300, width: 550 }}
-                      className="img-fluid"
-                      src={`${process.env.REACT_APP_API_RECURSOS}/recursos/ADHERITEDOSEP.png`}
-                      alt="Img"
-                    />
-                  </a>
-                </div>
-              </div>
-            </center>
-          </div>
-          <div className="pantallaChica">
-            <div className="ZonaDetalle-titulo ">
-              <h3 style={{ color: `#722789` }}>
-                INFORMACIÓN PARA PRESTADORES DE SERVICIOS:
-              </h3>
-            </div>
-
-            <center>
-              <div className="row">
-                {/*  <div className="one-third column">
-                    <Link to="/listado-guias-covid" className="link-menu">
-                      <img
-                        style={{ height: 200, width: 350 }}
-                        className="img-fluid"
-                        src={`${process.env.REACT_APP_API_RECURSOS}/recursos/modal/TARJETA_EXCURSIONES.jpg`}
-                        alt="Img"
-                      />
-                    </Link>
-    </div>*/}
-                <div className="one-two column">
-                  <a
-                    href="http://turismo.sanluis.gov.ar/dosep-turismo/Bases_Condiciones.pdf"
-                    className="link-menu"
-                  >
-                    <img
-                      style={{ height: 200, width: 400 }}
-                      className="img-fluid"
-                      src={`${process.env.REACT_APP_API_RECURSOS}/recursos/BASESYCONDICIONESDOSEP.png`}
-                      alt="Img"
-                    />
-                  </a>
-                </div>
-                <div className="one-two  column">
-                  <a
-                    href="https://forms.gle/wK9pGbWuNNTc2Z848"
-                    className="link-menu"
-                  >
-                    <img
-                      style={{ height: 200, width: 400 }}
-                      className="img-fluid"
-                      src={`${process.env.REACT_APP_API_RECURSOS}/recursos/ADHERITEDOSEP.png`}
-                      alt="Img"
-                    />
-                  </a>
-                </div>
               </div>
             </center>
           </div>
@@ -151,5 +34,3 @@ class PTurismoSemanaSanta extends Component {
     );
   }
 }
-
-export default PTurismoSemanaSanta;
