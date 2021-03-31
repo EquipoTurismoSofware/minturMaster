9b834bc6005b23da92ee8a28523821a247425193
fdamengual <fdamengual@ulp.edu.ar>
2021-03-31 11:42:28 -0300

    Centrar Flyer

--- minturMaster/src/pages/PTurismoSemanaSanta.js
+++ minturMaster/src/pages/PTurismoSemanaSanta.js
@@ -3,32 +3,22 @@ import React, { Component } from "react";
 export default class PTurismoSemanaSanta extends Component {
   render() {
     return (
-      <div className="container mb-5">
+      <div className="container mb-5" style={{ paddingTop: "150px" }}>
         <div className="container">
-          <div
-            className="row mb-3"
-            style={{
-              paddingTop: "150px",
-            }}
-          >
-            {/*<div className="ZonaDetalle-titulo">
+          {/*<div className="ZonaDetalle-titulo">
                     <h3 style={{ color: `#722789` }}>Semana Santa en San Luis:</h3>
                     </div>*/}
-            <center>
-              <div className="row">
-                <img
-                  style={{
-                    height: 700,
-                    width: 500,
-                    alignSelf: "center",
-                  }}
-                  className="img-fluid"
-                  src={`${process.env.REACT_APP_API_RECURSOS}/recursos/FolletoSemanaSanta.jpeg`}
-                  alt="Img"
-                />
-              </div>
-            </center>
-          </div>
+          <center>
+            <img
+              style={{
+                height: 700,
+                width: 500,
+              }}
+              className="img-fluid"
+              src={`${process.env.REACT_APP_API_RECURSOS}/recursos/FolletoSemanaSanta.jpeg`}
+              alt="Img"
+            />
+          </center>
         </div>
       </div>
     );
